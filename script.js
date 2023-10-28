0088073b808bb75d8ea2609a09b0be2806f10a87
Dayne Game <dayne@inboxdesign.co.nz>
2023-10-28 16:40:22 +1300

    Added More Stuff

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Grab the sidebar_close_btn
-let sidebar_close_btn = document.querySelector('.sidebar_close_btn');
+let sidebar_close_btn = document.querySelector('.sidebar_btn_container');
 // Grab the Sidebar
 let sidebar = document.querySelector('.sidebar');
 
