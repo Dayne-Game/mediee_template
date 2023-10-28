6558228947a4d7236010217421c67f80e76c9ed0
Dayne Game <dayne@Rachels-Mini.localdomain>
2023-10-28 16:02:07 +1300

    Template

---
+++ script.js
@@ -0,0 +1,8 @@
+// Grab the sidebar_close_btn
+let sidebar_close_btn = document.querySelector('.sidebar_close_btn');
+// Grab the Sidebar
+let sidebar = document.querySelector('.sidebar');
+
+sidebar_close_btn.addEventListener('click', function (event) {
+    sidebar.classList.toggle('close');
+})
